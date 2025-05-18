438831bf1c0ed533e5f515580afdc68f9c2d666d
ZidiEya <91125384+ZidiEya@users.noreply.github.com>
2025-05-18 04:30:50 +0100

    Update 3-private_password.dart

--- 0x02-dart-classes/3-private_password.dart
+++ 0x02-dart-classes/3-private_password.dart
@@ -1,13 +1,32 @@
 class Password {
-  String _password;
+  String _password = '';
 
-  Password({required String password}) : _password = password;
+  // Constructor to initialize password
+  Password({required String password}) {
+    _password = password;
+  }
 
   bool isValid() {
-    if (_password.length < 8 || _password.length > 16) return false;
-    if (!_password.contains(RegExp(r'[A-Z]'))) return false;
-    if (!_password.contains(RegExp(r'[a-z]'))) return false;
-    if (!_password.contains(RegExp(r'[0-9]'))) return false;
+    // Check password length
+    if (_password.length < 8 || _password.length > 16) {
+      return false;
+    }
+
+    // Check for at least one uppercase letter
+    if (!_password.contains(RegExp(r'[A-Z]'))) {
+      return false;
+    }
+
+    // Check for at least one lowercase letter
+    if (!_password.contains(RegExp(r'[a-z]'))) {
+      return false;
+    }
+
+    // Check for at least one digit
+    if (!_password.contains(RegExp(r'[0-9]'))) {
+      return false;
+    }
+
     return true;
   }
 
