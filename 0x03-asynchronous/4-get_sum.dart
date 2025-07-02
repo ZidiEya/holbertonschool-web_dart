16be893f674084df11e831c01887e59948b2e79e
Eya Zidi <91125384+ZidiEya@users.noreply.github.com>
2025-07-02 21:45:51 +0100

    Update 4-get_sum.dart

--- 0x03-asynchronous/4-get_sum.dart
+++ 0x03-asynchronous/4-get_sum.dart
@@ -1,49 +1,24 @@
-import 'dart:async';
 import 'dart:convert';
-
-Future<String> fetchUserOrders(String id) async {
-  var orders = {
-    "7ee9a243-01ca-47c9-aa14-0149789764c3": ["pizza", "orange"]
-  };
-  try {
-    return Future.delayed(
-        const Duration(seconds: 2), () => json.encode(orders[id]));
-  } catch (err) {
-    return "error caught : $err";
-  }
-}
-
-Future<String> fetchUserData() => Future.delayed(
-      const Duration(seconds: 2),
-      () =>
-          '{"id" : "7ee9a243-01ca-47c9-aa14-0149789764c3", "username" : "admin"}',
-    );
-
-Future<String> fetchProductPrice(product) async {
-  var products = {"pizza": 20.30, "orange": 10, "water": 5, "soda": 8.5};
-  try {
-    return Future.delayed(
-        const Duration(seconds: 2), () => json.encode(products[product]));
-  } catch (err) {
-    return "error caught : $err";
-  }
-}
+import '4-util.dart';
 
 Future<double> calculateTotal() async {
   try {
-    String userData = await fetchUserData();
-    final Map<String, dynamic> userMap = jsonDecode(userData);
-    String userId = userMap['id'];
+    // Fetch user data and decode it
+    final userData = await fetchUserData();
+    final user = jsonDecode(userData);
+    final userId = user['id'];
 
-    String ordersData = await fetchUserOrders(userId);
-    List<dynamic> ordersList = jsonDecode(ordersData);
+    // Fetch user orders and decode them
+    final ordersData = await fetchUserOrders(userId);
+    final orders = jsonDecode(ordersData);
 
     double total = 0.0;
-    for (var order in ordersList) {
-      String product = order.toString();
-      String priceData = await fetchProductPrice(product);
-      double productPrice = double.tryParse(priceData) ?? 0.0;
-      total += productPrice;
+
+    // Loop through each product and get its price
+    for (String product in orders) {
+      final priceData = await fetchProductPrice(product);
+      final price = jsonDecode(priceData);
+      total += price;
     }
 
     return total;
