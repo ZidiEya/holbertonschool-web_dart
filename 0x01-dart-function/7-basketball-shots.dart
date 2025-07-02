fce40343a2700de158ad9f53c2ec4dbc7ef04d74
Eya Zidi <91125384+ZidiEya@users.noreply.github.com>
2025-07-02 14:46:18 +0100

    Update 7-basketball-shots.dart

--- 0x01-dart-function/7-basketball-shots.dart
+++ 0x01-dart-function/7-basketball-shots.dart
@@ -1,16 +1,16 @@
 int whoWins(Map<String, int> teamA, Map<String, int> teamB) {
-  int calculateScore(Map<String, int> team) {
+  int getPoints(Map<String, int> team) {
     return (team['Free throws'] ?? 0) * 1 +
            (team['2 pointers'] ?? 0) * 2 +
            (team['3 pointers'] ?? 0) * 3;
   }
 
-  int scoreA = calculateScore(teamA);
-  int scoreB = calculateScore(teamB);
+  int scoreA = getPoints(teamA);
+  int scoreB = getPoints(teamB);
 
   if (scoreA > scoreB) {
     return 1;
-  } else if (scoreA < scoreB) {
+  } else if (scoreB > scoreA) {
     return 2;
   } else {
     return 0;
