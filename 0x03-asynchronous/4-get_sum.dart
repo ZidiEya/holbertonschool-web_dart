431557f4b6da120191f28489c54df6d775fedfd8
Eya Zidi <91125384+ZidiEya@users.noreply.github.com>
2025-07-02 21:42:08 +0100

    Update 4-get_sum.dart

--- 0x03-asynchronous/4-get_sum.dart
+++ 0x03-asynchronous/4-get_sum.dart
@@ -1,24 +1,49 @@
+import 'dart:async';
 import 'dart:convert';
-import '4-util.dart';
+
+Future<String> fetchUserOrders(String id) async {
+  var orders = {
+    "7ee9a243-01ca-47c9-aa14-0149789764c3": ["pizza", "orange"]
+  };
+  try {
+    return Future.delayed(
+        const Duration(seconds: 2), () => json.encode(orders[id]));
+  } catch (err) {
+    return "error caught : $err";
+  }
+}
+
+Future<String> fetchUserData() => Future.delayed(
+      const Duration(seconds: 2),
+      () =>
+          '{"id" : "7ee9a243-01ca-47c9-aa14-0149789764c3", "username" : "admin"}',
+    );
+
+Future<String> fetchProductPrice(product) async {
+  var products = {"pizza": 20.30, "orange": 10, "water": 5, "soda": 8.5};
+  try {
+    return Future.delayed(
+        const Duration(seconds: 2), () => json.encode(products[product]));
+  } catch (err) {
+    return "error caught : $err";
+  }
+}
 
 Future<double> calculateTotal() async {
   try {
-    // Get user data and parse ID
-    String userJson = await fetchUserData();
-    Map<String, dynamic> userMap = jsonDecode(userJson);
+    String userData = await fetchUserData();
+    final Map<String, dynamic> userMap = jsonDecode(userData);
     String userId = userMap['id'];
 
-    // Get user's orders and decode
-    String ordersJson = await fetchUserOrders(userId);
-    List<dynamic> orders = jsonDecode(ordersJson);
+    String ordersData = await fetchUserOrders(userId);
+    List<dynamic> ordersList = jsonDecode(ordersData);
 
-    double total = 0;
-
-    // For each product, fetch price and accumulate total
-    for (var product in orders) {
-      String priceJson = await fetchProductPrice(product);
-      double price = jsonDecode(priceJson).toDouble();
-      total += price;
+    double total = 0.0;
+    for (var order in ordersList) {
+      String product = order.toString();
+      String priceData = await fetchProductPrice(product);
+      double productPrice = double.tryParse(priceData) ?? 0.0;
+      total += productPrice;
     }
 
     return total;
